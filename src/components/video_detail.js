46475476254d48832950b647b14d9de1107ca321
Marcin Delektowski <marcin.delektowski@gmail.com>
2018-03-19 19:38:49 +0100

    Changing state to grumy cat.

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,23 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class EventClick extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { picSrc: 'https://whyevolutionistrue.files.wordpress.com/2017/06/happy-cat.jpg' };
-
+    this.picToggled = 'https://whyevolutionistrue.files.wordpress.com/2017/06/happy-cat.jpg';
+    this.state = { picSrc: this.picToggled };
+    this.counter = 0;
+    
   }
 
   render() {
     return (
       <img src={this.state.picSrc} className="kotek"
-      onClick={this.handleClick}
+        onClick={this.picturesToggling.bind(this)}
       />
     );
   }
 
   handleClick() {
-    this.setState({ picSrc: ''})
+    this.setState({ picSrc: this.picToggled })
+
+  }
+
+  picturesToggling() {
+    this.counter++;
+    // console.log(this.counter%2===0);
+    
+    if (this.counter%2===0) {
+      this.picToggled = 'https://whyevolutionistrue.files.wordpress.com/2017/06/happy-cat.jpg';
+      this.setState({ picSrc: this.picToggled });      
+    } else {
+        this.picToggled = 'https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/01/Grumpy-Cat-920x584.jpg';
+        this.setState({ picSrc: this.picToggled });
+      }
+    
+    
     
   }
 
