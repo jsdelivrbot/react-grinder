9166511b36a60bf64e2f75df7f7237fc14763a6c
Marcin Delektowski <marcin.delektowski@gmail.com>
2018-03-20 16:30:25 +0100

    vsc4

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const VideoListItem = (props) => {
+  console.log(props.video.snippet.thumbnails);
+  
   return <li>Video</li>
 }
 
-export default ListItem;
+export default VideoListItem;
